3739dbe97d6f13866478248aa6f5e564c6b3253c
Nikita Zayakin <nikita.zayakin@gmail.com>
2018-03-28 22:01:30 +0300

    remove .idea from repo`

--- 2.JavaCore/src/com/javarush/task/task14/task1408/Hen.java
+++ 2.JavaCore/src/com/javarush/task/task14/task1408/Hen.java
@@ -3,7 +3,7 @@ package com.javarush.task.task14.task1408;
 public abstract class Hen {
     abstract int getCountOfEggsPerMonth();
 
-    String getDescription(){
+    String getDescription() {
         return "Я - курица.";
     }
 }
